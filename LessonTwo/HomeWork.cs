298169b9d27bb282f85ef3d82d3eca5ee7e9fb68
Ekaterina Zhirnova <Katarinaetya4@gamil.com>
2023-03-10 20:16:19 +0300

    Add project files.

---
+++ LessonTwo/HomeWork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LessonTwo
+{
+    class WebSite
+    {
+        public string SiteName { get; set; } = "Example.com";
+        public string PathToSite { get; set; } = "https://www.example.com";
+        public string DescriptionOfSite { get; set; } = "about everything";
+        public string IPAddress { get; set; } = "195.19.255.1";
+        public void Print()
+        {
+            WriteLine($"\tСайт: {SiteName}\n\tПуть: {PathToSite}\n\tОписание сайта: {DescriptionOfSite}\n\tIP адрес сайта: {IPAddress}\n");
+        }
+    }
+    class Journal
+    {
+        public string JournalName { get; set; } = "Example name";
+        public int Year { get; set; } = 1990;
+        public string Email { get; set; } = "Example@mail.ru";
+        public void Print()
+        {
+            WriteLine($"\tНазвание журнала: {JournalName}\n\tГод основания: {Year}\n\tEmail для связи: {Email}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSite site1 = new WebSite
+            {
+                SiteName = "Dogs",
+                PathToSite = "https://www.dogs-are-my-friends.com",
+                DescriptionOfSite = "Here is information about dogs",
+                IPAddress = "172.16.255.2",
+            };
+            site1.Print();
+            Journal murzilka = new Journal
+            {
+                JournalName = "Мурзилка",
+                Year = 1924,
+                Email = "Murzilka@mail.ru"
+            };
+            murzilka.Print();
+        }
+    }
+}
---
+++ LessonTwo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LessonTwo
+{ 
+    class Student
+    {
+        public string FirstName { get; set; } = "John";
+        public string LastName { get; set; } = "Connor";
+        public string Group { get; set; } = "WPU221";
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+        public void Print()
+        {
+            WriteLine($"Имя: {FirstName}\n Фамилия: {LastName}\n Дата рождения: {BirthDate}\n Группа: {Group}\n");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student
+            {
+                FirstName = "Sahra",
+                LastName = "Connor",
+                BirthDate = new DateTime(1966, 3, 8),
+                Group = "T1000",
+            };
+            Student student2 = new Student
+            {
+                FirstName = "Arnold",
+                LastName = "Schwarznegger",
+                BirthDate = new DateTime(1946, 12, 23),
+                Group = "T800",
+            };
+            Student student3 = new Student
+            {
+                FirstName = "Robert",
+                LastName = "Patrik",
+                BirthDate = new DateTime(1966, 7, 18),
+                Group = "T1000",
+            };
+            Student student4 = new Student();
+            student1.Print();
+            student2.Print();
+            student3.Print();
+            student4.Print();
+        }
+    }
+}
